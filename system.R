d99a87d93c81097c515510cde92fe3356bff57e8
Konrad Rudolph <konrad.rudolph@gmail.com>
2013-12-08 13:57:21 +0000

    Added a `silent` command
    `silent` silences any messages from the enclosed expression which were
    sent to the standard output, by setting the `sink` locally.

--- system.R
+++ system.R
@@ -13,3 +13,10 @@ readcmd <- function (cmd, ...) {
 #mkdir <- syscmd('mkdir', flags = '-p', filename)
 mkdir <- function (what, flags = '-p')
     syscmd('mkdir', flags, what)
+
+#' Silences all output from an expression by redirecting the sink
+silent <- function (.expr) {
+    on.exit(sink())
+    sink(file = (if (Sys.info()['sysname'] == 'Windows') 'NUL' else '/dev/null'))
+    eval(.expr, envir = parent.frame())
+}
